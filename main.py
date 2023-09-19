3ff828f9c6e66709a386bc6193c9f3c9b3695ff0
kadynsky <kdnsk@protonmail.com>
2023-09-19 16:59:43 +0300

    Cleaned up some code.

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ timestamp = f"{year:02d}-{month:02d}-{day:02d}"
 PDF_DIRECTORY = "/home/hades/Books/pythonpractice/pdf_combinator/input"
 OUTPUT_PDF = "_output.pdf"
 
+def get_file_name(OUTPUT_PDF, timestamp):
+    stampname = f"{timestamp}{OUTPUT_PDF}"
+    RANDN = random.randint(100, 999)
+    stampname = f"{RANDN}{stampname}"
+    return(stampname)
+
 
 def collate_list(PDF_DIRECTORY):
     """
@@ -23,38 +29,56 @@ def collate_list(PDF_DIRECTORY):
     """
     pdf_files = [os.path.join(PDF_DIRECTORY, filename)
                  for filename in os.listdir(PDF_DIRECTORY) if filename.endswith(".pdf")] 
+
+    # Log message. Note: Add a proper log message.
     print('List collation DONE')
+    print(len(pdf_files))
     return(pdf_files)
 
 
 def compile_pdf(PDF_DIRECTORY, batch_size, pdf_files):
     """
-    Compiles each list with pdfs into pdf file
+    Compiles a list with pdfs according to batch_size and... 
+    Note: add pdf-drop if pdf_files is not divisible by batch_size.
     """
     i=0
-    print(pdf_files)
-    while pdf_files:
-        stampname = f"{timestamp}{OUTPUT_PDF}"
-        for file in pdf_files:
-            MERGE = PyPDF2.PdfMerger()
-            MERGE.append(file)
-            pdf_files.remove(file)
-            i+=1
-            if i >= batch_size:
-                RANDN = random.randint(100, 999)
-                stampname = f"{RANDN}{stampname}"
-                with open(stampname, "wb") as output:
-                    MERGE.write(output)
+    batch = []
+    for file in pdf_files:
+        batch.append(file)
+        i+=1
 
-                MERGE.close()
-            #print(f"PDF files in {PDF_DIRECTORY} have been compiled into {stampname} file.")
+        # If batch size is correct, then we call a write function.
+        if i >= batch_size:
+            write_batch(batch)
+            batch.clear()
+            i=0
 
-#def save_pdf(filename, pdf_object):
-    #if isinstane(pdf_object, PyPDF2.File
+
+def write_batch(batch): 
+    """
+    This function is called to write a given set of pdfs
+    """
+    print(len(batch))
+    print(f"{batch}  \n\n")
+
+    # Get a special name for new file and declare a MERGE function.
+    file_name = get_file_name(OUTPUT_PDF, timestamp)
+    MERGE = PyPDF2.PdfMerger()
+
+    # Append files from batch into PyPDF.FileMerge()
+    for batch_singles in batch:
+        MERGE.append(batch_singles)
+
+    # Write down batch.
+    with open(file_name, "wb") as output:
+        MERGE.write(output)
+    MERGE.close()
+
+    # Note: Add log message here.
 
 def run():
     pdf_list = collate_list(PDF_DIRECTORY)
-    compile_pdf(PDF_DIRECTORY, 5, pdf_list)
+    compile_pdf(PDF_DIRECTORY, 3, pdf_list)
 
 run()
 
