8b98f47ebe6f7da47f03f2fd136aea5c599c54e0
kadynsky <kdnsk@protonmail.com>
2023-09-20 10:19:47 +0300

    Added overflow if all pdfs don't fit equally into batches. Minor changes

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ import datetime
 import argparse
 import random
 
-# Compile a list of pdf files
-# Compiles related files into one (or more)
-# Saves compiled version into .
+'''
+Todo list:
+    - If batch_size doesn't divide pdf_list completely - dump the rest of pdf separately
+    either as another batch or single pdf files:
+        - Make so that you first do batches regularly and than you do oveflow_pdfs lastly.
+        And your list has to be sorted for that to meaningfully work.
+    - Organize funcitons.
+
+    - Build argparse options:
+        - First choice:
+            - Either the full directory to pdf merge Or select particular pdfs.
+        - Input options:
+            - Batch size. How many pdfs will be merged at a time.
+        - Choose the name for output file.
+        - Final confirmation menu. Either proceed or repeat above process.
+
+    - Write a settings.py for often used parameters.
+    - Wrap the programm in GUM.
+'''
 
 current = datetime.datetime.now()
 year = current.year
@@ -17,28 +33,37 @@ PDF_DIRECTORY = "/home/hades/Books/pythonpractice/pdf_combinator/input"
 OUTPUT_PDF = "_output.pdf"
 
 def get_file_name(OUTPUT_PDF, timestamp):
+    # Adds random three digits and year,month,day before the name.
     stampname = f"{timestamp}{OUTPUT_PDF}"
     RANDN = random.randint(100, 999)
     stampname = f"{RANDN}{stampname}"
     return(stampname)
 
 
-def collate_list(PDF_DIRECTORY):
+def collate_list(PDF_DIRECTORY, batch_size):
     """
     Searches directories for .pdf file only and then compiles them into lists
     """
     pdf_files = [os.path.join(PDF_DIRECTORY, filename)
-                 for filename in os.listdir(PDF_DIRECTORY) if filename.endswith(".pdf")] 
+                 for filename in os.listdir(PDF_DIRECTORY) if filename.endswith(".pdf")]
+
+    if len(pdf_files) % batch_size != 0:
+        overflow_pdfs = []
+        num = len(pdf_files) % batch_size
+        while num != 0:
+            overflow_pdfs.append(pdf_files[num])
+            num-=1
+        print(overflow_pdfs)
+        write_batch(overflow_pdfs)
 
     # Log message. Note: Add a proper log message.
     print('List collation DONE')
-    print(len(pdf_files))
     return(pdf_files)
 
 
-def compile_pdf(PDF_DIRECTORY, batch_size, pdf_files):
+def chop_batches(PDF_DIRECTORY, batch_size, pdf_files):
     """
-    Compiles a list with pdfs according to batch_size and... 
+    Compiles a list with pdfs according to batch_size and...
     Note: add pdf-drop if pdf_files is not divisible by batch_size.
     """
     i=0
@@ -54,12 +79,11 @@ def compile_pdf(PDF_DIRECTORY, batch_size, pdf_files):
             i=0
 
 
-def write_batch(batch): 
+def write_batch(batch):
     """
-    This function is called to write a given set of pdfs
+    This function is called to write a given set of pdfs.
+    You call it once per list of pdfs that you want to merge.
     """
-    print(len(batch))
-    print(f"{batch}  \n\n")
 
     # Get a special name for new file and declare a MERGE function.
     file_name = get_file_name(OUTPUT_PDF, timestamp)
@@ -77,11 +101,7 @@ def write_batch(batch):
     # Note: Add log message here.
 
 def run():
-    pdf_list = collate_list(PDF_DIRECTORY)
-    compile_pdf(PDF_DIRECTORY, 3, pdf_list)
+    pdf_list = collate_list(PDF_DIRECTORY, 3)
+    chop_batches(PDF_DIRECTORY, 3, pdf_list)
 
 run()
-
-
-
-
